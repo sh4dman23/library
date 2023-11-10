252c0f76bd629344ae7d71ea3d29bb9be5728534
Shadman <sh4dman0001@gmail.com>
2023-11-10 11:17:19 +0600

    Implement class syntax and prepend new books in ui list

--- script.js
+++ script.js
@@ -4,46 +4,19 @@ const addButton = document.querySelector('#addBook');
 const resetButton = document.querySelector('#resetForm');
 const counters = document.querySelectorAll('#total, #read');
 
-let userLibrary = [
-    new Book(
-        'Harry Potter',
-        'J.K. Rowling',
-        870,
-        'read'
-    ),
-    new Book(
-        'The Chalice of the Gods',
-        'Rick Riordan',
-        256,
-        'not read'
-    )
-];
-
-makeList();
-
-// Book object
-function Book(title, author, pages, status='not read') {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-}
-
-Book.prototype.toggleReadStatus = function() {
-    this.status = this.status === 'read' ? 'not read' : 'read';
-}
-
-// Update book list
-bookList.addEventListener('click', event => {
-    const target = event.target;
-
-    // Buttons are identified using classes
-    if (target.classList.contains('read-toggle')) {
-        toggleReadStatusOnButton(target);
-    } else if (target.classList.contains('remove-book')) {
-        removeBook(target);
+// Book Class
+class Book {
+    constructor(title, author, pages, status='not read') {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
     }
-});
+
+    toggleReadStatus() {
+        this.status = this.status === 'read' ? 'not read' : 'read';
+    }
+}
 
 function toggleReadStatusOnButton(button) {
     // First parentNode is buttons div, the next is the card
@@ -81,30 +54,6 @@ function updateCounters() {
     }, 0);
 }
 
-// Add books
-addButton.addEventListener('click', () => {
-    if (!checkForm()) {
-        return;
-    }
-
-    const title = document.querySelector('#book-title');
-    const author = document.querySelector('#book-author');
-    const pages = document.querySelector('#book-pages');
-    const readStatus = document.querySelector('#read-status');
-
-    addBookToLibrary(title.value, author.value, pages.value, readStatus.checked === true ? 'read' : 'not read');
-
-    resetForm();
-});
-
-resetButton.addEventListener('click', resetForm);
-
-document.querySelectorAll('input[type="text"], input[type="number"]').forEach(input => {
-    input.addEventListener('keyup', () => {
-        checkInput(input);
-    });
-});
-
 function resetForm() {
     addBookForm.reset();
     document.querySelectorAll('p.error').forEach(p => p.innerHTML = '');
@@ -128,20 +77,13 @@ function checkForm() {
 // Validates only one input at a time
 function checkInput(input, isErrorFree=true) {
     const errorNode = input.parentNode.querySelector('p.error');
+    const errSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>alert-circle</title><path d="M13,13H11V7H13M13,17H11V15H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>';
 
     if (!input.checkValidity() && input.id === 'book-pages') {
-        errorNode.innerHTML =
-        `
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>alert-circle</title><path d="M13,13H11V7H13M13,17H11V15H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>
-            Please provide a value between 1 and 100000!
-        `
+        errorNode.innerHTML = errSVG + ' Please provide a value between 1 and 100000!'
         isErrorFree = false;
     } else if (!input.checkValidity() || input.value.trim() === '') {
-        errorNode.innerHTML =
-        `
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>alert-circle</title><path d="M13,13H11V7H13M13,17H11V15H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>
-            This is a required field!
-        `
+        errorNode.innerHTML = errSVG + ' This is a required field';
         isErrorFree = false;
     } else {
         errorNode.innerHTML = '';
@@ -173,14 +115,28 @@ function makeList() {
 }
 
 function addBookCard(bookIndex) {
+    // Creates card section
+    function createCardPart(headerContent, mainContent) {
+        const div = document.createElement('div');
+        const header = document.createElement('h4');
+        header.textContent = headerContent;
+        div.appendChild(header);
+        const mainContentText = document.createTextNode(mainContent);
+        div.appendChild(mainContentText);
+        return div;
+    }
+
+    // Main card
     const bookCard = document.createElement('div');
     bookCard.classList.add('book');
     bookCard.setAttribute('data-attribute', bookIndex);
 
+    // Title
     const bookTitle = document.createElement('h3');
     bookTitle.classList.add('title');
     bookTitle.textContent = userLibrary[bookIndex].title;
 
+    // Middle part
     const bookAuthor = createCardPart('Author: ', userLibrary[bookIndex].author);
     const bookPages = createCardPart('Number of Pages: ', userLibrary[bookIndex].pages);
     const infoDiv = document.createElement('div');
@@ -188,6 +144,7 @@ function addBookCard(bookIndex) {
     infoDiv.appendChild(bookAuthor);
     infoDiv.appendChild(bookPages)
 
+    // Buttons
     const bookStatus = document.createElement('button');
     bookStatus.classList.add('read-toggle');
     if (userLibrary[bookIndex].status === 'read') {
@@ -206,20 +163,67 @@ function addBookCard(bookIndex) {
     buttons.appendChild(bookStatus);
     buttons.appendChild(removeButton);
 
+    // Join everything
     bookCard.appendChild(bookTitle);
     bookCard.appendChild(bookAuthor);
     bookCard.appendChild(bookPages);
     bookCard.appendChild(buttons);
 
-    bookList.appendChild(bookCard);
+    bookList.insertBefore(bookCard, bookList.firstElementChild);
 }
 
-function createCardPart(headerContent, mainContent) {
-    const div = document.createElement('div');
-    const header = document.createElement('h4');
-    header.textContent = headerContent;
-    div.appendChild(header);
-    const mainContentText = document.createTextNode(mainContent);
-    div.appendChild(mainContentText);
-    return div;
-}
+
+/* Initialization and Event Listeners */
+
+let userLibrary = [
+    new Book(
+        'Harry Potter',
+        'J.K. Rowling',
+        870,
+        'read'
+    ),
+    new Book(
+        'The Chalice of the Gods',
+        'Rick Riordan',
+        256,
+        'not read'
+    )
+];
+
+makeList();
+
+// Add books
+addButton.addEventListener('click', () => {
+    if (!checkForm()) {
+        return;
+    }
+
+    const title = document.querySelector('#book-title');
+    const author = document.querySelector('#book-author');
+    const pages = document.querySelector('#book-pages');
+    const readStatus = document.querySelector('#read-status');
+
+    addBookToLibrary(title.value, author.value, pages.value, readStatus.checked === true ? 'read' : 'not read');
+
+    resetForm();
+});
+
+resetButton.addEventListener('click', resetForm);
+
+document.querySelectorAll('input[type="text"], input[type="number"]').forEach(input => {
+    input.addEventListener('keyup', () => {
+        checkInput(input);
+    });
+});
+
+// Update book list
+bookList.addEventListener('click', event => {
+    const target = event.target;
+
+    // Buttons are identified using classes
+    if (target.classList.contains('read-toggle')) {
+        toggleReadStatusOnButton(target);
+    } else if (target.classList.contains('remove-book')) {
+        removeBook(target);
+    }
+});
